e9a32caf1e354c50206d36961e9a2df2915f9115
lcleveland <lyle.cleveland@yahoo.com>
2015-06-23 11:19:26 -0500

    Bug fixes related to command implementation and added another command

---
+++ SoloBot.IRC.Plugins.Commands.Say/Say.cs
@@ -0,0 +1,70 @@
+﻿using IrcMessageSharp;
+using SoloBot.IRC.Command.Interface;
+using SoloBot.IRC.Interface;
+using System;
+using System.ComponentModel.Composition;
+
+namespace SoloBot.IRC.Plugins.Commands.Say
+{
+    [Export(typeof(IIRCCommand))]
+    public class Say : IIRCCommand
+    {
+        public string Command
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void ReceiveRawCommand(IIRCPlugin sender, string command)
+        {
+            var message = IrcMessage.Parse(command);
+            foreach (var item in message.Params)
+            {
+                if (item.StartsWith("!say"))
+                {
+                    sender.SendCommand("privmsg " + sender.Channel + " : " + item.Substring(item.IndexOf(' ') + 1));
+                }
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Version
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
